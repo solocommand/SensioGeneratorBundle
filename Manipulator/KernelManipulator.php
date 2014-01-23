688f6664b57b4714c5df436cab2e0875167281ff
Josh Worden <josh.worden@cygnus.com>
2014-01-23 09:00:44 -0600

    Added method to enable multiple bundles at once.

--- Manipulator/KernelManipulator.php
+++ Manipulator/KernelManipulator.php
@@ -102,4 +102,87 @@ class KernelManipulator extends Manipulator
             }
         }
     }
+
+    /**
+     * Adds multiple bundles at the end of the existing ones.
+     * Due to Manipulator using the ReflectionObject, changes to the source of a
+     * file are not reflected if addBundle is used within a loop.
+     *
+     * @param array $bundle The bundle class names to add
+     *
+     * @return Boolean true if it worked, false otherwise
+     *
+     * @throws \RuntimeException If bundle is already defined
+     */
+    public function addBundles($bundles = array())
+    {
+        if (!$this->reflected->getFilename()) {
+            return false;
+        }
+
+        $src = file($this->reflected->getFilename());
+        $method = $this->reflected->getMethod('registerBundles');
+        $lines = array_slice($src, $method->getStartLine() - 1, $method->getEndLine() - $method->getStartLine() + 1);
+
+        $bundleSrc = "";
+
+        // Don't add same bundle twice
+        foreach ($bundles as $bundle) {
+
+            if (false !== strpos(implode('', $lines), $bundle)) {
+                continue;
+            }
+
+            $bundleSrc .= sprintf("            new %s(),\n", $bundle);
+
+        }
+
+        if ("" == $bundleSrc) {
+            throw new \RuntimeException('All bundles already defined in "AppKernel::registerBundles()".');
+        }
+
+        $this->setCode(token_get_all('<?php '.implode('', $lines)), $method->getStartLine());
+        while ($token = $this->next()) {
+            // $bundles
+            if (T_VARIABLE !== $token[0] || '$bundles' !== $token[1]) {
+                continue;
+            }
+
+            // =
+            $this->next();
+
+            // array
+            $token = $this->next();
+            if (T_ARRAY !== $token[0]) {
+                return false;
+            }
+
+            // add the bundle at the end of the array
+            while ($token = $this->next()) {
+                // look for );
+                if (')' !== $this->value($token)) {
+                    continue;
+                }
+
+                if (';' !== $this->value($this->peek())) {
+                    continue;
+                }
+
+                // ;
+                $this->next();
+
+                $lines = array_merge(
+                    array_slice($src, 0, $this->line - 2),
+                    // Appends a separator comma to the current last position of the array
+                    array(rtrim(rtrim($src[$this->line - 2]), ',') . ",\n"),
+                    array($bundleSrc),
+                    array_slice($src, $this->line - 1)
+                );
+
+                file_put_contents($this->reflected->getFilename(), implode('', $lines));
+
+                return true;
+            }
+        }
+    }
 }
